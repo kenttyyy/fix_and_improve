65815aa9c68b38eb441d061e0fab2bda259e685f
kenttyyy <kentajrins123@gmail.com>
2025-03-12 23:25:21 +0800

    main part

--- two_number_print_bigger.py
+++ two_number_print_bigger.py
@@ -1,4 +1,20 @@
 # Create the variable for the numbers and input for the user
 
-num_one = int(input("Please enter the first number"))
-num_two = int(input("Please enter the second number"))
+try:
+    num_one = int(input("Please enter the first number "))
+    num_two = int(input("Please enter the second number "))
+
+# Main part of the program
+    if num_one > num_two:
+        print(f"{num_one}")
+    elif num_one < num_two:
+        print(f"{num_two}")
+    else:
+        print(f"{num_one} and {num_two} are equal")
+
+# Added as a means of verifying an invalid input
+except ValueError:
+    print("Please input a whole number")
+
+
+
