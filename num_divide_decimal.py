3ede62ab2beb79f55d0dafa811db62aaf13fb401
kenttyyy <kentajrins123@gmail.com>
2025-03-13 10:18:32 +0800

    division

--- num_divide_decimal.py
+++ num_divide_decimal.py
@@ -1,3 +1,15 @@
 # Ask user input
-num_1 = float(input("Please input the first number: "))
-num_2 = float(input("Please input the second number: "))
+try:
+    num_1 = float(input("Please input the first number: "))
+    num_2 = float(input("Please input the second number: "))
+
+# Calculates the division,
+    if num_2 == 0:
+        print("Division by zero is not allowed")
+    else:
+        print(f"The quotient is: {num_1 / num_2}")\
+
+except ValueError:
+    print("Please input a valid number")
+
+print("Thanks for using the program!")
