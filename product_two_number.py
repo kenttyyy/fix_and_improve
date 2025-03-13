2e3532b9604b80f81e900ca476df4e1788c6f3f8
kenttyyy <kentajrins123@gmail.com>
2025-03-13 09:41:16 +0800

    product

--- product_two_number.py
+++ product_two_number.py
@@ -1,3 +1,11 @@
 # Ask the user to input two number
-num_one = float(input("Please input the first number: "))
-num_two = float(input("Please input the second number: "))
+try:
+    num_one = float(input("Please input the first number: "))
+    num_two = float(input("Please input the second number: "))
+
+# Calculates for the product
+    print(f"The product is: {num_one * num_two}")
+
+# Mostly add value error, just in case invalid
+except ValueError:
+    print("Please input a valid number")
