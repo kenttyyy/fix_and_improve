8d67ff60ba06c60bd68681f1281d54852a04c210
kenttyyy <kentajrins123@gmail.com>
2025-03-13 12:01:52 +0800

    subtract formula

--- ten_num_minus_first.py
+++ ten_num_minus_first.py
@@ -5,3 +5,10 @@ print("This program calculates the result of subtracting all numbers from the fi
 num_1 = float(input("Please input the first number: "))
 total = num_1
 
+# Ten numbers that will be used
+for i in range(9):
+    num = float(input(f"Enter number {i + 2}: "))
+    total -= num
+
+# Remaining after subtraction
+print(f"Result after subtraction: {total}")
