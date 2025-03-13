f82a77d2b42e87831b9af0e41ccbfc344c0ef524
kenttyyy <kentajrins123@gmail.com>
2025-03-13 11:45:03 +0800

    ask input

---
+++ two_num_quotient.py
@@ -0,0 +1,6 @@
+# Explain program
+print("This program calculates the quotient of two numbers without the decimal point.")
+
+# Enter number
+num_1 = int(input("Please input the first number: "))
+num_2 = int(input("Please input the second number: "))
