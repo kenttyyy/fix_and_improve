0cc1bdf95188f73b34a75c23b2c09072edbb002f
kenttyyy <kentajrins123@gmail.com>
2025-03-13 17:47:56 +0800

    store and prompt

---
+++ continuous_highest_lowest.py
@@ -0,0 +1,8 @@
+# Explain the program
+print("This program sorts all entered numbers from highest to lowest.")
+
+# List to store input numbers
+numbers = []
+
+# Prompt to enter a number
+numbers.append(int(input("Enter a number: ")))
