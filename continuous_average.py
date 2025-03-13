5660d037c4b657a8ac511c60097b0ab18d42672d
kenttyyy <kentajrins123@gmail.com>
2025-03-13 18:00:38 +0800

    calculate average

--- continuous_average.py
+++ continuous_average.py
@@ -4,4 +4,16 @@ print("This program calculates the average of entered numbers.")
 # List to store input numbers
 numbers = []
 
-numbers.append(float(input("Enter a number: ")))
+try:
+    while True:
+        numbers.append(float(input("Enter a number: ")))
+
+except ValueError:
+    if numbers:
+# Calculate the average by dividing the sum by the count
+        average = sum(numbers) / len(numbers)
+        print(f"The average of the numbers entered is: {average:.2f}")
+    else:
+        print("No numbers were entered.")
+
+print("Thanks for using the program!")
