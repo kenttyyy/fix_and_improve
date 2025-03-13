9e8ad82d88ba23f5f93283aae776850cd6ca3c3e
kenttyyy <kentajrins123@gmail.com>
2025-03-13 11:37:57 +0800

    ask input

---
+++ two_num_diff.py
@@ -0,0 +1,6 @@
+# Explain the programm
+print("This program calculates the difference of two numbers.")
+
+# User input
+num_1 = float(input("Please input the first number: "))
+num_2 = float(input("Please input the second number: "))
