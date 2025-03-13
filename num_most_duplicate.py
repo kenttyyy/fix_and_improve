6361d7a66fab929fc04276fb25c1c0459e46cfe2
kenttyyy <kentajrins123@gmail.com>
2025-03-13 17:27:29 +0800

    highest count

--- num_most_duplicate.py
+++ num_most_duplicate.py
@@ -5,6 +5,17 @@ print("This program finds the number with the most duplicates from your inputs."
 numbers = {}
 
 # Increment count of entered number
-while True:
-    num = int(input("Enter a number: "))
-    numbers[num] = numbers.get(num, 0) + 1
+try:
+    while True:
+        num = int(input("Enter a number: "))
+        numbers[num] = numbers.get(num, 0) + 1
+
+except ValueError:
+    if numbers:
+        # Find the number with the highest count using max()
+        most_frequent = max(numbers, key=numbers.get)
+        print(f"The number with the most duplicates is: {most_frequent}")
+    else:
+        print("No numbers were entered.")
+
+print("Thanks for using the program!")
