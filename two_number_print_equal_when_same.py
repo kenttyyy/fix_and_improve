55dbdabb43452db142b1b0e32429613c7308466b
kenttyyy <kentajrins123@gmail.com>
2025-03-13 09:04:03 +0800

    main part

--- two_number_print_equal_when_same.py
+++ two_number_print_equal_when_same.py
@@ -1,6 +1,12 @@
 # Ask the user to input two number
-num_one = int(input("Input the first number "))
-num_two = int(input("Input the second number "))
+try:
+    num_one = float(input("Input the first number "))
+    num_two = float(input("Input the second number "))
 
+# Checks if the number is equal
+    if num_one == num_two:
+        print("Equal")
 
-
+except ValueError:
+    print("Please enter a whole number")
+ 
