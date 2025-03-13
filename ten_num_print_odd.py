4572b3d2bb60429041db684ecccdc224662b6d11
kenttyyy <kentajrins123@gmail.com>
2025-03-13 10:51:20 +0800

    total odd

--- ten_num_print_odd.py
+++ ten_num_print_odd.py
@@ -5,7 +5,16 @@ print("This program counts how many odd numbers are entered.")
 count = 0
 
 # Prompts the user to enter 10 numbers
-for i in range(10):
-    num = int(input(f"Enter number {i + 1}: "))
-    if num % 2 != 0:
-        count += 1
+try:
+    for i in range(10):
+        num = int(input(f"Enter number {i + 1}: "))
+        if num % 2 != 0:
+            count += 1
+
+# Shows the odd numberss
+    print(f"Total odd numbers: {count}")
+
+except ValueError:
+    print("Please input a valid integer")
+
+print("Thanks for using the program!")
