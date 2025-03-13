5b6f375386bba61d5aeedb13c9825ad1d4cd9653
kenttyyy <kentajrins123@gmail.com>
2025-03-13 11:41:14 +0800

    difference

--- two_num_diff.py
+++ two_num_diff.py
@@ -2,5 +2,13 @@
 print("This program calculates the difference of two numbers.")
 
 # User input
-num_1 = float(input("Please input the first number: "))
-num_2 = float(input("Please input the second number: "))
+try:
+    num_1 = float(input("Please input the first number: "))
+    num_2 = float(input("Please input the second number: "))
+
+    print(f"The difference is: {num_1 - num_2}")
+
+except ValueError:
+    print("Please input a valid number")
+
+print("Thanks for using the program!")
