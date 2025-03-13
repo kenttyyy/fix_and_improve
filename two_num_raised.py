fdf68a8c95a14d4f37e60e9d4daf6ad0f3d745f9
kenttyyy <kentajrins123@gmail.com>
2025-03-13 10:24:08 +0800

    ask input

---
+++ two_num_raised.py
@@ -0,0 +1,7 @@
+# Explains what the program do
+print("This program calculates the result of raising the first number to the power of the second number.")
+
+# Ask to input two numbers
+num_1 = float(input("Please input the base number: "))
+num_2 = float(input("Please input the exponent: "))
+
