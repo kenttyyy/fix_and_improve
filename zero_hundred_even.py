14da50b8c7be725080278cf032fd9d083725aa80
kenttyyy <kentajrins123@gmail.com>
2025-03-13 10:56:38 +0800

    all even

---
+++ zero_hundred_even.py
@@ -0,0 +1,8 @@
+# Explain how the program works
+print("This program prints all even numbers from 0 to 100.")
+
+# print zero to 100 excluding odd
+for i in range(0, 101, 2):
+    print(i)
+
+print("Thanks for using the program!")
