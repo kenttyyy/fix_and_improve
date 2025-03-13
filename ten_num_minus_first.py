85f6d4fb58910ceebf973384e35f76303546c434
kenttyyy <kentajrins123@gmail.com>
2025-03-13 12:04:08 +0800

    result

--- ten_num_minus_first.py
+++ ten_num_minus_first.py
@@ -2,13 +2,19 @@
 print("This program calculates the result of subtracting all numbers from the first number.")
 
 # Enter the first number
-num_1 = float(input("Please input the first number: "))
-total = num_1
+try:
+    num_1 = float(input("Please input the first number: "))
+    total = num_1
 
 # Ten numbers that will be used
-for i in range(9):
-    num = float(input(f"Enter number {i + 2}: "))
-    total -= num
+    for i in range(9):
+        num = float(input(f"Enter number {i + 2}: "))
+        total -= num
 
 # Remaining after subtraction
-print(f"Result after subtraction: {total}")
+    print(f"Result after subtraction: {total}")
+
+except ValueError:
+    print("Please input a valid number")
+
+print("Thanks for using the program!")
