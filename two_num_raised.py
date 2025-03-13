9b2d954179ed0b0fd416a844b1c84ef2f1357a96
kenttyyy <kentajrins123@gmail.com>
2025-03-13 10:28:25 +0800

    raised formula

--- two_num_raised.py
+++ two_num_raised.py
@@ -2,6 +2,14 @@
 print("This program calculates the result of raising the first number to the power of the second number.")
 
 # Ask to input two numbers
-num_1 = float(input("Please input the base number: "))
-num_2 = float(input("Please input the exponent: "))
+try:
+    num_1 = float(input("Please input the base number: "))
+    num_2 = float(input("Please input the exponent: "))
 
+# Do the necessary calculation when raised
+    print(f"The result is: {num_1 ** num_2}")
+
+except ValueError:
+    print("Please input a valid number")
+
+print("Thanks for using the program!")
