51751a7b478ced1f04ae123d900656e7e764d271
kenttyyy <kentajrins123@gmail.com>
2025-03-13 11:51:50 +0800

    two number

---
+++ two_num_remainder.py
@@ -0,0 +1,6 @@
+# Explain program
+print("This program calculates the remainder when the first number is divided by the second number.")
+
+# Enter two numbers
+num_1 = int(input("Please input the first number: "))
+num_2 = int(input("Please input the second number: "))
