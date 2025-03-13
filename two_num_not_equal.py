ee7cfa9bb91d719a6a171dc862b049e1898fa1fa
kenttyyy <kentajrins123@gmail.com>
2025-03-13 11:34:30 +0800

    not equal

--- two_num_not_equal.py
+++ two_num_not_equal.py
@@ -2,5 +2,15 @@
 print("This program checks if two numbers are not equal.")
 
 # Prompt to input numbers
-num_1 = float(input("Please input the first number: "))
-num_2 = float(input("Please input the second number: "))
+try:
+    num_1 = float(input("Please input the first number: "))
+    num_2 = float(input("Please input the second number: "))
+
+# Checks if it is equal or not
+    if num_1 != num_2:
+        print("Not Equal")
+
+except ValueError:
+    print("Please input a valid number")
+
+print("Thanks for using the program!")
