d42a89f1fd80e2ce2e0e96cbaee5c239516e45ad
kenttyyy <kentajrins123@gmail.com>
2025-03-13 11:48:06 +0800

    without decimal

--- two_num_quotient.py
+++ two_num_quotient.py
@@ -2,5 +2,17 @@
 print("This program calculates the quotient of two numbers without the decimal point.")
 
 # Enter number
-num_1 = int(input("Please input the first number: "))
-num_2 = int(input("Please input the second number: "))
+try:
+    num_1 = int(input("Please input the first number: "))
+    num_2 = int(input("Please input the second number: "))
+
+# Perform division in two numbers
+    if num_2 == 0:
+        print("Division by zero is not allowed")
+    else:
+        print(f"The quotient is: {num_1 // num_2}")
+
+except ValueError:
+    print("Please input a valid integer")
+
+print("Thanks for using the program!")
