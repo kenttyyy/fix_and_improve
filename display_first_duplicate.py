35b84761225cad57035aa9367437015023e02602
kenttyyy <kentajrins123@gmail.com>
2025-03-13 18:43:04 +0800

    track

---
+++ display_first_duplicate.py
@@ -0,0 +1,6 @@
+# Explain program
+print("This program shows numbers, but duplicates appear only once.")
+
+def first_occurrences():
+    nums = []  # List to store the unique numbers.
+    seen = set()  # Set to track numbers that have been seen before.
