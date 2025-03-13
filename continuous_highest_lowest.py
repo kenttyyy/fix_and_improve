f06f1b1b9ff33a782905ca9b95281e2676545dcf
kenttyyy <kentajrins123@gmail.com>
2025-03-13 17:51:58 +0800

    sort and print

--- continuous_highest_lowest.py
+++ continuous_highest_lowest.py
@@ -5,4 +5,15 @@ print("This program sorts all entered numbers from highest to lowest.")
 numbers = []
 
 # Prompt to enter a number
-numbers.append(int(input("Enter a number: ")))
+try:
+    while True:
+        numbers.append(int(input("Enter a number: ")))
+except ValueError:
+    if numbers:
+        # Sort numbers in descending order
+        numbers.sort(reverse=True)
+        print("Numbers from highest to lowest:", ", ".join(map(str, numbers)))
+    else:
+        print("No numbers were entered.")
+
+print("Thanks for using the program!")
