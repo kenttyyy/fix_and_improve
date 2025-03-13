122da7f8b99745be6636ad1c0d5327ce550f4b50
kenttyyy <kentajrins123@gmail.com>
2025-03-13 17:40:16 +0800

    print highest

--- continuous_highest.py
+++ continuous_highest.py
@@ -5,9 +5,17 @@ print("This program finds the highest number from your inputs.")
 highest = None
 
 # Prompt to enter number
-while True:
-    num = int(input("Enter a number: "))
+try:
+    while True:
+        num = int(input("Enter a number: "))
 
 # Update highest if it's the first number or if num is greater
-    if highest is None or num > highest:
-        highest = num
+        if highest is None or num > highest:
+            highest = num
+except ValueError:
+        if highest is not None:
+            print(f"The highest number entered is: {highest}")
+        else:
+            print("No numbers were entered.")
+
+print("Thanks for using the program!")
