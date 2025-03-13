2e4d1dcbd180d13e71dfd239ab48cfed9cfb0153
kenttyyy <kentajrins123@gmail.com>
2025-03-13 11:06:36 +0800

    input num

---
+++ two_num_small.py
@@ -0,0 +1,6 @@
+# Explain the program
+print("This program prints the smaller number based on the input of the user.")
+
+# Prompt to input two numbers
+num_1 = float(input("Please input the first number: "))
+num_2 = float(input("Please input the second number: "))
