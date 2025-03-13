e9d55284bf625354bdfa5ee8a9dce4172663b424
kenttyyy <kentajrins123@gmail.com>
2025-03-13 12:18:31 +0800

    odd formula

---
+++ zero_hundred_odd.py
@@ -0,0 +1,10 @@
+# Explain program
+print("This program prints all odd numbers from 0 to 100.")
+
+# Start from 1 since 0 is even
+num = 1
+while num <= 100:
+    print(num)
+    num += 2
+
+print("Thanks for using the program!")
