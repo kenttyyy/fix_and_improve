fe3dd38ced97a95fd6062e49338a93584a3a93d9
kenttyyy <kentajrins123@gmail.com>
2025-03-13 11:54:38 +0800

    with remainder

--- two_num_remainder.py
+++ two_num_remainder.py
@@ -2,5 +2,17 @@
 print("This program calculates the remainder when the first number is divided by the second number.")
 
 # Enter two numbers
-num_1 = int(input("Please input the first number: "))
-num_2 = int(input("Please input the second number: "))
+try:
+    num_1 = int(input("Please input the first number: "))
+    num_2 = int(input("Please input the second number: "))
+
+# Divides and get the remainder
+    if num_2 == 0:
+        print("Division by zero is not allowed")
+    else:
+        print(f"The remainder is: {num_1 % num_2}")
+
+except ValueError:
+    print("Please input a valid integer")
+
+print("Thanks for using the program!")
