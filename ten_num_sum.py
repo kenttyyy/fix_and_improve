354c795fc2a6fe77fd9e7a66ca72bb9bbdd49287
kenttyyy <kentajrins123@gmail.com>
2025-03-13 10:41:15 +0800

    total

--- ten_num_sum.py
+++ ten_num_sum.py
@@ -5,6 +5,13 @@ print("This program calculates the sum of 10 numbers.")
 total = 0
 
 # This makes the continuous enter of 10 numbers
-for i in range(10):
-    num = float(input(f"Enter number {i + 1}: "))
-    total += num
+try:
+    for i in range(10):
+        num = float(input(f"Enter number {i + 1}: "))
+        total += num
+    print(f"The total sum is: {total}")
+
+except ValueError:
+    print("Please input a valid number")
+
+print("Thanks for using the program!")
