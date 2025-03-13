e1abd8bc58148654c62ba3b91020d08fa82f0ad6
kenttyyy <kentajrins123@gmail.com>
2025-03-13 11:58:59 +0800

    first number

---
+++ ten_num_minus_first.py
@@ -0,0 +1,7 @@
+# Explain program
+print("This program calculates the result of subtracting all numbers from the first number.")
+
+# Enter the first number
+num_1 = float(input("Please input the first number: "))
+total = num_1
+
