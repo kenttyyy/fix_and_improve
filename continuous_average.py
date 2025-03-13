c3eed44df03d244612ed1e997fc9d085d2a13ef4
kenttyyy <kentajrins123@gmail.com>
2025-03-13 17:56:18 +0800

    store prompt

---
+++ continuous_average.py
@@ -0,0 +1,7 @@
+# Explain the program
+print("This program calculates the average of entered numbers.")
+
+# List to store input numbers
+numbers = []
+
+numbers.append(float(input("Enter a number: ")))
