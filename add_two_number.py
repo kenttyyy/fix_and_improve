57f421466d2a85f0f159ef2455fa89d90f79f115
kenttyyy <kentajrins123@gmail.com>
2025-03-13 09:18:27 +0800

    addition

--- add_two_number.py
+++ add_two_number.py
@@ -1,4 +1,10 @@
 # Ask user to input two numbers
-num_one = float(input("Please input the first number: "))
-num_two = float(input("Please input the second number: "))
+try:
+    num_one = float(input("Please input the first number: "))
+    num_two = float(input("Please input the second number: "))
 
+# Calculates the sum
+    print(f"The sum is: {num_one + num_two}")
+
+except ValueError:
+    print("Please input a valid number")
